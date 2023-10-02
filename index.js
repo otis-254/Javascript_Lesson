1cbd3129f068f8f93fa87e117e60c97c8b8e197d
Caleb Olele <olelecaleb176@gmail.com>
2023-10-02 13:25:14 +0300

    Javascript Lesson Code

---
+++ index.js
@@ -0,0 +1,64 @@
+ 
+//         //This is my veruy first JS code
+//         // console.log('Hello World');
+//     // let name ='Calleb' + 'Otieno';
+//     console.log(name);
+
+//     let message ='My name is '
+//     // let firstName = 'Kamau';
+//     let secondName = 'Omondi';
+//     console.log(message , firstName, secondName);
+
+
+  
+// // alert("Do you really want to leave this page\n Please Click OK to confirm");
+
+// // var user = prompt("Please enter your name first")
+// // alert(user)
+
+// const interestRate = 0.3
+ 
+// console.log(interestRate)
+
+
+// let name = 'Olele'; //String
+// let age = 20 //Number
+// let isApproved = true; //Booleon
+// let firstName = undefined //
+
+
+
+//Notations
+let person = {
+    name : 'Okello',
+    age : 30, 
+}
+person.age = 30;
+console.log(person.age);
+
+//Arrays
+let selectedColors = ['Red' , 'Blue' , 'Green'];
+selectedColors [3] = 1000 
+console.log (selectedColors)
+
+
+let cars = ['V8' ,'Ford', 'Lamb.' ,'Subaru']
+console.log(cars)
+
+
+//Funstions
+function greet (name, lastName) {
+    console.log ('Hello '+ name + ' ' +  lastName);
+}
+
+greet('Kevin' ,  'Opondo'); 
+
+//Calculating a value
+
+function square (number){
+    return number* number
+}
+
+let number = square[2]
+console.log (square[2]);
+
